28c983dbfc5685ca0fca81c81ba361f4752bdfa5
zafar iqbal <68896404+Zafar-Iqbal-Khan@users.noreply.github.com>
2022-11-12 10:22:45 +0530

    first commit

---
+++ lib/pages/home/components/education_section.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:web_portfolio/models/education.dart';
+import 'package:web_portfolio/utils/constants.dart';
+import 'package:web_portfolio/utils/screen_helper.dart';
+
+final List<Education> educationList = [
+  Education(
+    description: "Working as a FullStack Developer",
+    linkName: "www.flutterpanda.com",
+    period: "2021 - PRESENT",
+  ),
+  Education(
+    description:
+        "Bachelors in Technology - Computer Science From SSM College Of Engineering And Technology , ParihasPora Pattan,Kashmir",
+    linkName: "www.flutterpanda.com",
+    period: "2017 - 2021",
+  ),
+  Education(
+    description: "11TH-12TH From Govt. Higher Secondary School Tral,Kashmir",
+    linkName: "www.flutterpanda.com",
+    period: "2013 - 2015",
+  ),
+  Education(
+    description: "9TH-10TH Class From Hajahad Public School Awantipora,Kashmir",
+    linkName: "www.flutterpanda.com",
+    period: "2011 - 2013",
+  ),
+];
+
+class EducationSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ScreenHelper(
+        desktop: _buildUi(kDesktopMaxWidth),
+        tablet: _buildUi(kTabletMaxWidth),
+        mobile: _buildUi(getMobileMaxWidth(context)),
+      ),
+    );
+  }
+
+  Widget _buildUi(double width) {
+    return Container(
+      alignment: Alignment.center,
+      child: ResponsiveWrapper(
+        maxWidth: width,
+        minWidth: width,
+        defaultScale: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "EDUCATION",
+              style: GoogleFonts.oswald(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+                fontSize: 30.0,
+                height: 1.3,
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Wrap(
+              children: [
+                Container(
+                  constraints: BoxConstraints(maxWidth: 400.0),
+                  child: Text(
+                    "A full stack all round developer that does all the job he needs to do at all times. Actually this is a false statement",
+                    style: TextStyle(
+                      color: Colors.white,
+                      height: 1.5,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return Container(
+                  child: Wrap(
+                    spacing: 20.0,
+                    runSpacing: 20.0,
+                    children: educationList
+                        .map(
+                          (education) => Container(
+                            width: constraints.maxWidth / 2.0 - 20.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  education.period,
+                                  style: GoogleFonts.oswald(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Text(
+                                  education.description,
+                                  maxLines: 4,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: kCaptionColor,
+                                    height: 1.5,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                MouseRegion(
+                                  cursor: SystemMouseCursors.click,
+                                  child: GestureDetector(
+                                    onTap: () {},
+                                    child: Text(
+                                      education.linkName,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 40.0,
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
